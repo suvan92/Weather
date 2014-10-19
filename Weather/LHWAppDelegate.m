c2fd54a7710c6be42e70c89b91097755edf7f10f
dannylmathews <dannyl.mathews@gmail.com>
2014-10-18 20:57:18 -0700

    removed dictionaries.

--- Weather/LHWAppDelegate.m
+++ Weather/LHWAppDelegate.m
@@ -13,14 +13,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     
-    NSDictionary *cities = @{@"Tokyo": @{@"Temperature": @"14", @"Chance of rain": @"10%", @"Humidity": @"0.3"},
-                             @"Chicago": @{@"Temperature": @"25", @"Chance of rain": @"4%", @"Humidity": @"0.4"},
-                             @"Vancouver": @{@"Temperature": @"5", @"Chance of rain": @"90%", @"Humidity": @"0.5"},
-                             @"Cape Town": @{@"Temperature": @"31", @"Chance of rain": @"78%", @"Humidity": @"0.1"},
-                             @"Ulan Bator": @{@"Temperature": @"26", @"Chance of rain": @"33%", @"Humidity": @"0.2"}};
-    NSMutableArray *cityVCs = [NSMutableArray array];
-    
-    
     return YES;
 }
 
